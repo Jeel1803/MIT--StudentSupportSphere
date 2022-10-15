ea20dd8bbdfd3294b3c85f6aedbfb63b2a75a0c8
Jeel1803 <jeel.khadsaliya1803@gmail.com>
2022-10-15 16:19:48 +1300

    [View Grades]: onSelectListener added. Marks of assignment changes as per selection.

--- app/src/main/java/com/example/mitapplication/viewGrades.java
+++ app/src/main/java/com/example/mitapplication/viewGrades.java
@@ -3,12 +3,18 @@ package com.example.mitapplication;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -22,8 +28,11 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,15 +40,12 @@ import java.util.List;
 public class viewGrades extends AppCompatActivity {
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
-    FirebaseUser user;
     String userId;
-    TextView Course;
-    ArrayAdapter<String> adapter;
-
-
-     Spinner spinner;
+    Spinner spinner;
     List<String> courses = new ArrayList<>();
-
+    RecyclerView marksList;
+    TextView assignment1, assignment2, test, marks1, marks2, testMarks, totalMarks;
+    String courseDoc;
 
 
     @Override
@@ -48,11 +54,17 @@ public class viewGrades extends AppCompatActivity {
         setContentView(R.layout.activity_view_grades);
         fAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
-        user = fAuth.getCurrentUser();
         userId = fAuth.getCurrentUser().getUid();
-        Course = findViewById(R.id.course);
         spinner = findViewById(R.id.spinner);
+        assignment1 = findViewById(R.id.assignment1);
+        assignment2 = findViewById(R.id.assignment2);
+        test = findViewById(R.id.test);
+        marks1 = findViewById(R.id.marks1);
+        marks2 = findViewById(R.id.marks2);
+        testMarks = findViewById(R.id.testMarks);
+        totalMarks = findViewById(R.id.totalMarks);
 
+        //marksList = findViewById(R.id.marksList);
 
 
         CollectionReference df = fStore.collection("students").document(userId).collection("Course");
@@ -68,24 +80,45 @@ public class viewGrades extends AppCompatActivity {
                         courses.add(course);
                     }
                     adapter.notifyDataSetChanged();
+                    spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            String item = parent.getSelectedItem().toString();
+                            df
+                                    .whereEqualTo("Name", item)
+                                    .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                            for (QueryDocumentSnapshot document : task.getResult()){
+                                                String assignemnt = document.getData().get("Assignment1").toString();
+                                                marks1.setText(assignemnt + " / 30");
+                                                String assignment2 = document.getData().get("Assignment2").toString();
+                                                marks2.setText(assignment2 + " / 40") ;
+                                                String test = document.getData().get("Test").toString();
+                                                testMarks.setText(test + " / 30");
+
+
+                                            }
+                                        }
+                                    });
+
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+
+                        }
+                    });
+
                 }
+
+
             }
         });
-
-
-
-
-
-
-
-
-
-
-
     }
-
-
-
-
-
 }
+
+
+
+
+
